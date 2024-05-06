63f6ed7d5dd98518f2528338997100609f1f3734
finesaaa <indriatifiqey16@gmail.com>
2024-05-06 12:11:50 +0700

    fix: change interval to 10 minutes

--- dags/trending_youtube_dag_sqlite.py
+++ dags/trending_youtube_dag_sqlite.py
@@ -18,7 +18,7 @@ default_args = {
     default_args=default_args,
     description='A pipeline to fetch trending YouTube videos into SQLite db',
     start_date=datetime(2023, 5, 7, tzinfo=timezone(timedelta(hours=7))),
-    schedule_interval='0 10 * * *',
+    schedule_interval='10 * * * *',
     catchup=False)
 def trending_youtube_dag():
     @task()
